f493236ba225eeb3cd15e981c5b00c359cc5c1ef
Fifo (Fabricius Zatti) <62725221+fazzatti@users.noreply.github.com>
2024-06-18 19:34:34 -0300

    Export types (#6) (#7)
    * docs: improve readme
    
    * Create main.yml (#1)
    
    * package: add jsr.json (#2)
    
    * Feat jsr (#3)
    
    * package: add jsr.json
    
    * fix: jsr path
    
    * feat: switch to deno (#4)
    
    * Switch to Deno (#5)
    
    * feat: switch to deno
    
    * fix uuid generation
    
    * feat: add export types
    
    * packaging: bump version to 0.1.1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
 export { ProcessEngine } from "./process-engine/index.ts";
+export type * from "./process-engine/types.ts";
+
+export { MetadataHelper } from "./metadata/collector/index.ts";
+export type * from "./metadata/collector/types.ts";
+
+export { ConveeError } from "./error/index.ts";
+export type * from "./error/types.ts";
+
+export type * from "./core/types.ts";
+
+export type * from "./belt-plugin/types.ts";
+

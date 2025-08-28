2a35798d46b9334d82f47a01515b72c91f3c6511
Fifo (Fabricius Zatti) <62725221+fazzatti@users.noreply.github.com>
2025-08-28 16:07:17 -0300

    fix: Error objects losing custom properties (#21)

--- src/error/types.ts
+++ src/error/types.ts
@@ -2,7 +2,6 @@ import { EngineMetadata } from "../core/types.ts";
 
 export interface IConveeError<ErrorT extends Error> extends Error {
   engineStack: EngineMetadata[];
-  sourceError: ErrorT;
 
   enrichConveeStack: (metadata: EngineMetadata) => IConveeError<ErrorT>;
 }

78b8450b6f8d6cc09aca6247783058f3ad1f5033
Fabricius Zatti <fazzatti@gmail.com>
2025-10-03 17:36:27 -0300

    feat: add type guard for ProcessEngine to validate object structure

---
+++ src/utils/types/is-process-engine.ts
@@ -0,0 +1,7 @@
+import { ProcessEngine } from "../../process-engine/types.ts";
+
+export const isProcessEngine = <I, O, E extends Error>(
+  value: unknown
+): value is ProcessEngine<I, O, E> => {
+  return typeof value === "object" && value !== null && "run" in value;
+};

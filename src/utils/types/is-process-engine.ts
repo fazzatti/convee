87c414543b05f0f873c511197df1d6fbbb0cfab1
Fabricius Zatti <fazzatti@gmail.com>
2025-10-06 11:50:32 -0300

    feat: enhance isProcessEngine type guard to check if 'run' is a function

--- src/utils/types/is-process-engine.ts
+++ src/utils/types/is-process-engine.ts
@@ -3,5 +3,10 @@ import { ProcessEngine } from "../../process-engine/types.ts";
 export const isProcessEngine = <I, O, E extends Error>(
   value: unknown
 ): value is ProcessEngine<I, O, E> => {
-  return typeof value === "object" && value !== null && "run" in value;
+  return (
+    typeof value === "object" &&
+    value !== null &&
+    "run" in value &&
+    value.run instanceof Function
+  );
 };

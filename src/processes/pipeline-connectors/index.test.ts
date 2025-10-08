ee8305144b0f73985ddddaa48e7d4a0393b8ec26
Fifo (Fabricius Zatti) <62725221+fazzatti@users.noreply.github.com>
2025-10-08 15:16:00 -0300

    refactor: replace storeOutput with storeMetadata and update tests acc… (#27)

--- src/processes/pipeline-connectors/index.test.ts
+++ src/processes/pipeline-connectors/index.test.ts
@@ -8,7 +8,7 @@ import {
 import { Pipeline } from "../../pipeline/index.ts";
 import { ProcessEngine } from "../../process-engine/index.ts";
 import { MetadataHelper } from "../../metadata/collector/index.ts";
-import { storeOutput, StoreOutputConnector } from "./store-output.ts";
+import { storeMetadata } from "./store-metadata.ts";
 
 const stringToNumberPlusOneTransformer: Transformer<string, number> = async (
   item: string
@@ -24,7 +24,7 @@ const numberToStringEngine = ProcessEngine.create((n: number) => n.toString(), {
   name: "numberToStringEngine",
 });
 
-Deno.test("Pipeline store output connector", async () => {
+Deno.test("Pipeline store metadata connector with step arg", async () => {
   let metadataHelper: MetadataHelper | undefined;
 
   const extractedMetadataHelperStep = <T>(
@@ -46,15 +46,15 @@ Deno.test("Pipeline store output connector", async () => {
   const pipeline = Pipeline.create(
     [
       doubleNumberModifier,
-      storeOutput(doubleNumberModifier, "first"),
+      storeMetadata("first", doubleNumberModifier),
       numberToObject,
-      storeOutput(numberToObject, "second"),
+      storeMetadata("second", numberToObject),
       objToNumber,
-      storeOutput(objToNumber, "third"),
+      storeMetadata("third", objToNumber),
       numberToStringEngine,
-      storeOutput(numberToStringEngine, "fourth"),
+      storeMetadata("fourth", numberToStringEngine),
       stringToNumberPlusOneTransformer,
-      storeOutput(stringToNumberPlusOneTransformer, "fifth"),
+      storeMetadata("fifth", stringToNumberPlusOneTransformer),
       extractedMetadataHelperStep,
     ],
     {
@@ -88,3 +88,140 @@ Deno.test("Pipeline store output connector", async () => {
     assertEquals(metadataHelper.get("fifth"), 11);
   }
 });
+
+Deno.test("Pipeline store metadata connector without step arg", async () => {
+  let metadataHelper: MetadataHelper | undefined;
+
+  const extractedMetadataHelperStep = <T>(
+    input: T,
+    metadata?: MetadataHelper
+  ): T => {
+    metadataHelper = metadata;
+    return input;
+  };
+
+  type obj = { n: number; tag: string };
+
+  const numberToObject = (item: number) => {
+    return { n: item, tag: "custom" };
+  };
+
+  const objToNumber = (input: { n: number; tag: string }): number => {
+    return input.n;
+  };
+
+  const pipeline = Pipeline.create(
+    [
+      doubleNumberModifier,
+      storeMetadata<number>("first"),
+      numberToObject,
+      storeMetadata<obj>("second"),
+      objToNumber,
+      storeMetadata<number>("third"),
+      numberToStringEngine,
+      storeMetadata<string>("fourth"),
+      stringToNumberPlusOneTransformer,
+      storeMetadata<number>("fifth"),
+      extractedMetadataHelperStep,
+    ],
+    {
+      name: "TestPipeline",
+      id: "test123",
+      plugins: [],
+    }
+  );
+
+  const firstResult = await pipeline.run(10);
+
+  assertEquals(firstResult, 21);
+  assertExists(metadataHelper);
+  if (metadataHelper) {
+    assertEquals(metadataHelper.get("first"), 20);
+    assertEquals(metadataHelper.get("second"), { n: 20, tag: "custom" });
+    assertEquals(metadataHelper.get("third"), 20);
+    assertEquals(metadataHelper.get("fourth"), "20");
+    assertEquals(metadataHelper.get("fifth"), 21);
+  }
+
+  const secondResult = await pipeline.run(5);
+
+  assertEquals(secondResult, 11);
+  assertExists(metadataHelper);
+  if (metadataHelper) {
+    assertEquals(metadataHelper.get("first"), 10);
+    assertEquals(metadataHelper.get("second"), { n: 10, tag: "custom" });
+    assertEquals(metadataHelper.get("third"), 10);
+    assertEquals(metadataHelper.get("fourth"), "10");
+    assertEquals(metadataHelper.get("fifth"), 11);
+  }
+});
+
+Deno.test(
+  "Pipeline store metadata connector with mix of step arg and just key",
+  async () => {
+    let metadataHelper: MetadataHelper | undefined;
+
+    const extractedMetadataHelperStep = <T>(
+      input: T,
+      metadata?: MetadataHelper
+    ): T => {
+      metadataHelper = metadata;
+      return input;
+    };
+
+    type obj = { n: number; tag: string };
+
+    const numberToObject = (item: number) => {
+      return { n: item, tag: "custom" };
+    };
+
+    const objToNumber = (input: { n: number; tag: string }): number => {
+      return input.n;
+    };
+
+    const pipeline = Pipeline.create(
+      [
+        doubleNumberModifier,
+        storeMetadata("first", doubleNumberModifier),
+        numberToObject,
+        storeMetadata<obj>("second"),
+        objToNumber,
+        storeMetadata<number>("third"),
+        numberToStringEngine,
+        storeMetadata<string>("fourth"),
+        stringToNumberPlusOneTransformer,
+        storeMetadata("fifth", stringToNumberPlusOneTransformer),
+        extractedMetadataHelperStep,
+      ],
+      {
+        name: "TestPipeline",
+        id: "test123",
+        plugins: [],
+      }
+    );
+
+    const firstResult = await pipeline.run(10);
+
+    assertEquals(firstResult, 21);
+    assertExists(metadataHelper);
+    if (metadataHelper) {
+      assertEquals(metadataHelper.get("first"), 20);
+      assertEquals(metadataHelper.get("second"), { n: 20, tag: "custom" });
+      assertEquals(metadataHelper.get("third"), 20);
+      assertEquals(metadataHelper.get("fourth"), "20");
+      assertEquals(metadataHelper.get("fifth"), 21);
+    }
+
+    const secondResult = await pipeline.run(5);
+
+    assertEquals(secondResult, 11);
+    assertExists(metadataHelper);
+    if (metadataHelper) {
+      assertEquals(metadataHelper.get("first"), 10);
+      assertEquals(metadataHelper.get("second"), { n: 10, tag: "custom" });
+      assertEquals(metadataHelper.get("third"), 10);
+      assertEquals(metadataHelper.get("fourth"), "10");
+      assertEquals(metadataHelper.get("fifth"), 11);
+    }
+  }
+);
